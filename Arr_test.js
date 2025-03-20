45d54ca11f40f4127d243d6e4f0971762d062f18
hamsu12 <hamsj3808@gmail.com>
2025-03-20 17:40:29 +0900

    중급영상 : 45

--- Arr_test.js
+++ Arr_test.js
@@ -2,3 +2,13 @@ let arr=[1, 2, 3, 4, 5];
 let result= arr.splice(1, 2, 200, 300);
 console.log(arr);
 console.log(result);
+
+let arr2 = ['Mike', 'Tom', 'Hamsu'];
+arr2.forEach((name, index) => {
+    console.log(`${index + 1}. ${name}`);
+});
+//순서 출력
+
+let arr3=[1, 2, 3, 4, 5];
+let result3 = arr3.indexOf(3);
+console.log(result3);
