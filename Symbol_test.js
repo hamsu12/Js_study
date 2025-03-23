ab95c3692f41af534b14ee40694c8c2f7846fdc7
hamsu12 <hamsj3808@gmail.com>
2025-03-24 00:02:36 +0900

    중급영상 : 1:06:33

--- Symbol_test.js
+++ Symbol_test.js
@@ -1,2 +1,12 @@
 //Symbol : 유일성 보장
 //Symbol.for() : 전역 심볼
+
+const id1 = Symbol('id');
+const id2 = Symbol('id');
+
+console.log(id1 === id2); //false
+console.log(id1 == id2); //false
+
+const id3 = Symbol('id');
+const id4 = Symbol
+
